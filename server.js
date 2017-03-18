da4a5baf7aa61005f9f73316e1dcf92e809feefd
Rutger Prins <rutger@kiezo.nl>
2017-03-18 12:20:41 +0100

    Some modifications

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/static/')).listen(8080, function(){
-    console.log('Server running on http://localhost:8080/ ...');
+connect().use(serveStatic(__dirname + '/static/')).listen(80, function(){
+    console.log('Server running on http://wifiprobes.bitsofgroningen.nl:80/ ...');
 });
